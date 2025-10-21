1810ad61de6b5ce5b5ff35d4a0cfb73be8866beb
Hellnep <prime@hellnep.ru>
2025-10-22 07:45:46 +1000

    Изменение маршрута в верхний уровень.

--- Web/Program.cs
+++ Web/Program.cs
@@ -10,13 +10,11 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 
 app.UseHttpsRedirection();
+
 app.UseDeveloperExceptionPage();
 app.UseRouting();
 
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllers();
-});
+app.MapControllers();
 
 app.Run();
 
