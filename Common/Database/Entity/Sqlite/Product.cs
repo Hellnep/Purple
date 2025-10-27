d39c17a80a90c612bdf3415df8c2ee352c212b23
Hellnep <prime@hellnep.ru>
2025-10-28 00:52:01 +1000

    Простое представление модели продуктов

--- Common/Database/Entity/Sqlite/Product.cs
+++ Common/Database/Entity/Sqlite/Product.cs
@@ -1,8 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Purple.Common.Database.Entity.Sqlite;
 
 public class Product
 {
+    [Key]
+    public int Id { get; set; }
 
+    [Required]
+    [StringLength(25)]
+    public required string Name { get; set; }
+
+    [StringLength(100)]
+    public string? Description { get; set; } = null!;
 }
