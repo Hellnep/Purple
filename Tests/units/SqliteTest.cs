b4fb3e4cd5543c9538a205095c094c4645ec826b
Hellnep <prime@hellnep.ru>
2025-10-28 20:13:00 +1000

    Тестирование функционирования базы данных.

--- Tests/units/SqliteTest.cs
+++ Tests/units/SqliteTest.cs
@@ -1,16 +1,61 @@
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Infrastructure;
 
+using Purple.Common.Database.Entity.Sql;
 using Purple.Common.Database.Context.Sqlite;
 
 namespace Purple.Tests.Unit;
 
 public class SqliteTest
 {
+    SqliteConnection connection;
+    DbContextOptions<PurpleOcean> options;
+
+    public SqliteTest()
+    {
+        connection = new SqliteConnection("Data Source=:memory:");
+        connection.Open();
+
+        options = new DbContextOptionsBuilder<PurpleOcean>()
+            .UseSqlite(connection)
+            .Options;
+    }
+
     [Fact]
-    public void Initialize()
+    public void Adding_a_buyer()
     {
         // Arrange
-        
+        using (PurpleOcean context = new PurpleOcean(options))
+        {
+            // Act
+            context.Customers.Add(
+                new Customer { Id = 1, Username = "Hellnep", Date = DateOnly.FromDateTime(DateTime.Now) }
+            );
+            context.SaveChanges();
+
+            // Assert
+            Assert.NotNull(context.Customers.Single(customer => customer.Id == 1));
+        }
+    }
+
+    [Fact]
+    public void Adding_a_product()
+    {
+        // Arrange
+        using (PurpleOcean context = new PurpleOcean(options))
+        {
+            // Act
+            context.Products.AddRange(
+                new Product { Id = 1, Name = "Milk" },
+                new Product { Id = 2, Name = "Orange", Description = "It's fucking orange" }
+            );
+            context.SaveChanges();
+
+            // Assert
+            Assert.NotNull(context.Products);
+            Assert.Equal(2, context.Products.Count());
+            Assert.Null(context.Products.Single(products => products.Id == 1).Description);
+
+        }
     }
 }
