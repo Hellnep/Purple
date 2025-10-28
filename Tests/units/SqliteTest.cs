d23eca1a9b0025090f9a388ba3c2d8de3655d3fd
Hellnep <prime@hellnep.ru>
2025-10-28 19:50:25 +1000

    Удаление старого проекта тестов, и добавление нового.

---
+++ Tests/units/SqliteTest.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+using Purple.Common.Database.Context.Sqlite;
+
+namespace Purple.Tests.Unit;
+
+public class SqliteTest
+{
+    [Fact]
+    public void Initialize()
+    {
+        // Arrange
+        
+    }
+}
