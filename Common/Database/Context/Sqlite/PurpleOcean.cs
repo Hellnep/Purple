753f78a39285b4a855972200e989e318f2ec53d7
Hellnep <prime@hellnep.ru>
2025-10-28 16:10:40 +1000

    Создание простой базы данных

--- Common/Database/Context/Sqlite/PurpleOcean.cs
+++ Common/Database/Context/Sqlite/PurpleOcean.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Purple.Common.Database.Entity.Sqlite;
+﻿using Purple.Common.Database.Entity.Sqlite;
+
+using Microsoft.EntityFrameworkCore;
 
 namespace Purple.Common.Database.Context.Sqlite;
 
 public class PurpleOcean : DbContext
 {
-    
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Product> Products { get; set; }
+
+    public PurpleOcean(DbContextOptions<PurpleOcean> options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
 }
