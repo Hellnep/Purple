1a1eb675acf7851a42320a4cdf0d55e8135f6d57
Hellnep <prime@hellnep.ru>
2025-10-31 08:45:36 +1000

    Adding attributes for columns properties

--- Common/Database/Entity/Sql/Product.cs
+++ Common/Database/Entity/Sql/Product.cs
@@ -1,16 +1,20 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Purple.Common.Database.Entity.Sql;
 
+[Table("Products")]
 public class Product
 {
     [Key]
     public long Id { get; set; }
 
     [Required]
-    [StringLength(25)]
+    [MinLength(5), StringLength(40)]
+    [Display(Name = "Title")]
     public required string Name { get; set; }
 
-    [StringLength(100)]
+    [MaxLength(200)]
+    [Column("ProductDescription")]
     public string? Description { get; set; } = null!;
 }
